42aaf6d64410a1ac7c6e28a18c9c38fea18c18ed
Adrián Lorenzo <soldonfull@gmail.com>
2021-12-03 11:47:27 +0000

    added game

---
+++ UQuiz/Assets/Scripts/HandManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandManager : MonoBehaviour
+{
+    [SerializeField] InputActionReference controllerActionGrip;
+    [SerializeField] InputActionReference controllerActionTrigger;
+
+    private Animator _handAnimator;
+
+    void Awake() {
+        controllerActionGrip.action.performed += GripPress;
+        controllerActionTrigger.action.performed += TriggerPress;
+
+        _handAnimator = GetComponent<Animator>();
+    }
+
+    private void GripPress(InputAction.CallbackContext obj) {
+        _handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+    }
+
+    private void TriggerPress(InputAction.CallbackContext obj) {
+        _handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+    }
+}
